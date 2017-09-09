57fa3b14e3384e3ba84f2504c605b64809f286ce
Ronaldo Felipe <ronaldolocalhost@gmail.com>
2017-09-09 16:29:01 -0400

    Fixes in the library folders

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,15 +1,32 @@
 (function(){
-    function Start(){
-
+    let stage:createjs.Stage;
+    let canvas:any;
+    let helloLabel:createjs.Text;
+  
+    function Start() {
+      canvas = document.getElementById("canvas");
+      stage = new createjs.Stage(canvas);
+      createjs.Ticker.framerate = 60;
+      createjs.Ticker.on("tick", Update);
+      Main();
     }
-
+  
     function Update() {
-        
+      helloLabel.rotation += 5;
+      stage.update();
     }
-    
+  
     function Main() {
-        
+      console.log("Game Started...");
+  
+      helloLabel = new createjs.Text("Hello, World!", "40px Consolas", "#000000");
+      helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+      helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+      helloLabel.x = 320;
+      helloLabel.y = 240;
+      stage.addChild(helloLabel);
     }
-
+  
     window.onload = Start;
-})()
+  
+  })();
